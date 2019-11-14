b6fa389dbd7b3ecfca9e52f56ae62e85b644805c
tiyst <tiystw@gmail.com>
2019-11-15 00:50:12 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/POS_Snake.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/tiyst/Documents/Programming/cpp/POS_Snake/main.cpp" "C:/Users/tiyst/Documents/Programming/cpp/POS_Snake/cmake-build-debug/CMakeFiles/POS_Snake.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/POS_Snake.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS_Snake.dir/main.cpp.obj"
+  "POS_Snake.exe"
+  "POS_Snake.exe.manifest"
+  "POS_Snake.pdb"
+  "libPOS_Snake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POS_Snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
